58c5008a91bfc5cf84a5de4b304d34f54dfddd73
Cédric Teyton <cedric.teyton@promyze.com>
2021-10-08 12:34:11 +0200

    Step 5 : proxy handling

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function startServer() {
         url: ':url',
         http_version: ':http-version',
         remote_addr: ':remote-addr',
+        remote_addr_forwarded: ':req[x-forwarded-for]', //Get a specific header
         response_time: ':response-time',
         status: ':status',
         content_length: ':res[content-length]',
