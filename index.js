95a23825e9988cd29d7e8b27541bc52bb25f20c8
Cédric Teyton <cedric.teyton@promyze.com>
2021-10-08 12:53:06 +0200

    Step 6 : send to ES

--- index.js
+++ index.js
@@ -3,13 +3,35 @@ const http = require('http');
 const morgan = require('morgan');
 const winston = require('winston');
 const userAgentParser = require('ua-parser-js');
+const winstonElasticsearch = require('winston-elasticsearch');
+
+require('dotenv').config()
+
+const esTransportOpts = {
+    level: 'info',
+    indexPrefix: 'logging-api',
+    indexSuffixPattern: 'YYYY-MM-DD',
+    clientOpts : {
+        node: process.env.ES_ADDON_URI,
+        maxRetries: 5,
+        requestTimeout: 10000,
+        sniffOnStart: false,
+        auth: {
+            username: process.env.ES_ADDON_USER,
+            password: process.env.ES_ADDON_PASSWORD
+        }
+    },
+    source: process.env.LOG_SOURCE || 'api'
+};
+const esTransport = new winstonElasticsearch.ElasticsearchTransport(esTransportOpts);
 
 const logger = winston.createLogger({
     transports: [
         new winston.transports.Console({
             level: 'info',
             json: true
-        })
+        }),
+        esTransport //Add es transport
     ]
 });
 
